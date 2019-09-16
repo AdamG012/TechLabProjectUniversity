8f2e3497ad3104425552d629f8b557829bf9df9f
Cameron Williamson <cwilliamson@transformd.com>
2019-09-16 10:54:58 +1000

    make contactform a controlled component

--- code/client/src/components/ContactForm.js
+++ code/client/src/components/ContactForm.js
@@ -4,40 +4,71 @@ class ContactForm extends React.Component {
   constructor(props) {
     super(props);
     this.contactFormRef = React.createRef();
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      content: ""
+    };
   }
   handleFormSubmit = e => {
     e.preventDefault();
     console.log("form submitted");
   };
 
+  handleNameChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleSubjectChange = e => {
+    this.setState({ subject: e.target.value });
+  };
+
+  handleContentChange = e => {
+    this.setState({ content: e.target.value });
+  };
+
   render() {
+    const { name, email, subject, content } = this.state;
     return (
       <div ref={this.contactFormRef} className="contact-form" id="contact-form">
         <h4 className="h4">Contact</h4>
         <form className="contact-form__form" onSubmit={this.handleFormSubmit}>
           <input
+            onChange={this.handleNameChange}
             className="contact-form__input contact-form__input--small"
             name="name"
             type="text"
             placeholder="Name"
+            value={name}
           />
           <input
+            onChange={this.handleEmailChange}
             className="contact-form__input contact-form__input--small"
             name="email"
             type="text"
             placeholder="Email"
+            value={email}
           />
           <input
+            onChange={this.handleSubjectChange}
             className="contact-form__input contact-form__input--small"
             name="subject"
             type="text"
             placeholder="Subject"
+            value={subject}
           />
           <textarea
+            onChange={this.handleContentChange}
             className="contact-form__input contact-form__input--large"
             name="content"
             rows="8"
             placeholder="Type your message here..."
+            value={content}
           />
           <button type="submit">Submit</button>
         </form>
