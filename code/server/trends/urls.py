f198bc1669616b283b32314968b9c718df3747a4
clmo6615 <clmo6615@uni.sydney.edu.au>
2019-09-21 21:25:55 +1000

    URL paths for API change

--- code/server/trends/urls.py
+++ code/server/trends/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('search', views.search, name='search'),
-    path(r'^articles/(?P<article_id>[0-9]+)/$', views.article, name='article')
-    path('latest-articles', views.latest_articles, name='latest-articles')
-    path('abstract', views.article_abstract, name='abstract')
+    path(r'^articles/(?P<article_id>[0-9]+)/$', views.article, name='article'),
+    path('latest-articles', views.latest_articles, name='latest-articles'),
+    path('abstract', views.article_abstract, name='abstract'),
 ]
