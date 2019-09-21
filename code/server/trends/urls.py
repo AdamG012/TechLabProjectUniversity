8b1349ea737e97d7fc1d70a030b1d5f8ba0009fa
clmo6615 <clmo6615@uni.sydney.edu.au>
2019-09-21 21:24:05 +1000

    URL paths for API change

--- code/server/trends/urls.py
+++ code/server/trends/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('index', views.index, name='index'),
     path('search', views.search, name='search'),
     path(r'^articles/(?P<article_id>[0-9]+)/$', views.article, name='article')
+    path('latest-articles', views.latest_articles, name='latest-articles')
+    path('abstract', views.article_abstract, name='abstract')
 ]
