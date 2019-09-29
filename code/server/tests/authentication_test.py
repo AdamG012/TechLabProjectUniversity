362427ebfa21d8be9ea0a6a1271f0f229835d761
AdamG <windows8@localhost.localdomain>
2019-09-29 14:18:32 +1000

    Added tests for logout

--- code/server/tests/authentication_test.py
+++ code/server/tests/authentication_test.py
@@ -1,27 +1,51 @@
-import unittest
-import code.server.trends.db.admin.authentication as auth;
+from django.test import TestCase, tag
+from unittest import TestCase,main
+from code.server.trends.db.admin.authentication import login, logout
+
+#           Authentication
+
+# - Testing for login and logout
+# - Determining functionality proper authentication of users.
+# - To run all these tests use:
+#       python manage.py test server
+# - To run these tests specifically as unit tests e.g. run by tag.
+#       python manage.py test server --tag=unit
 
 
-class MyTestCase(unittest.TestCase):
+class Authentication(TestCase):
 
+    @tag('unit', 'auth')
     def test_invalid_login_none(self):
-        self.assertFalse(auth.login(None, None, None))
+        self.assertFalse(login(None, None, None))
 
+    @tag('unit', 'auth')
     def test_invalid_login_none_username(self):
-        self.assertFalse(auth.login("Hello", None, "password"))
+        self.assertFalse(login("Hello", None, "password"))
 
+    @tag('unit', 'auth')
     def test_invalid_login_none_password(self):
-        self.assertFalse(auth.login("Hello", None, "password"))
+        self.assertFalse(login("Hello", "user", None))
 
+    @tag('unit', 'auth')
     def test_invalid_login_none_request(self):
-        self.assertFalse(auth.login("Hello", None, "password"))
+        self.assertFalse(login(None, "user", "password"))
 
+    @tag('unit', 'auth')
     def test_invalid_login_wrong_password(self):
-        self.assertFalse(auth.login("clintbot", None, "fakePassword"))
+        self.assertFalse(login("clintbot", "user", "fakePassword"))
 
+    @tag('unit', 'auth')
     def test_invalid_login_unknown_username(self):
-        self.assertFalse(auth.login("fakeUser", None, "password"))
+        self.assertFalse(login("fakeUser", "user1", "password"))
 
+    @tag('unit', 'auth')
+    def test_invalid_logout_none(self):
+        self.assertFalse(logout(None))
 
-if __name__ == '__main__':
-    unittest.main()
+    @tag('unit', 'auth')
+    def test_invalid_logout_already_logged_in(self):
+        self.assertFalse(logout("Hello"))
+
+    @tag('unit', 'auth')
+    def test_valid_logout(self):
+        self.assertTrue(logout("fakeUser"))
