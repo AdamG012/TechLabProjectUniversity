ad03d655309b96e66c42d24ff2e1ca53a5dbc638
AdamG <windows8@localhost.localdomain>
2019-10-13 17:35:49 +1100

    Added a footer navigation component

---
+++ code/client/src/components/FooterNavigation.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const footerLinks = [
+    {
+        text: "Home",
+        toPath: "/",
+        requiresAuth: false
+    },
+    {
+        text: "Meet Us",
+        toPath: "/about",
+        requiresAuth:false
+    },
+    {
+        text: "Privacy Policy",
+        toPath: "/privacy_policy",
+        requiresAuth: false
+    },
+    {
+        text: "Terms of Use",
+        toPath: "/terms",
+        requiresAuth: false
+    },
+    {
+        text: "Admin",
+        toPath: "/admin_login",
+        requiresAuth: false
+    }
+];
+
+const renderLinks = linkData => {
+    return linkData.map(link => {
+        return (
+            <li className="navigation__list-item">
+                <Link className="navigation__link" to={link.toPath}>
+                    {link.text}
+                </Link>
+            </li>
+        );
+    });
+};
+
+const FooterNavigation = () => {
+    return (
+        <div className="navigation-container">
+            <nav className="navigation">
+                <ul className="navigation__list">{renderLinks(footerLinks)}</ul>
+            </nav>
+        </div>
+    );
+};
+
+export default FooterNavigation;
