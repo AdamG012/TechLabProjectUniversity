2e3b93701b786a2661c0f926e1e548a1e7916c9c
Cameron Williamson <cwilliamson@transformd.com>
2019-10-21 11:24:56 +1100

    fix contact form bug

--- code/client/src/components/GetInTouch.js
+++ code/client/src/components/GetInTouch.js
@@ -1,24 +1,39 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 const HEADER_OFFSET = 80;
 
-const GetInTouch = props => {
-  const contactFormReference = props.contactRef;
-  return (
-    <div
-      className="get-in-touch"
-      onClick={() => {
-        window.scrollTo(
-          0,
-          contactFormReference.current.contactFormRef.current.offsetTop -
-            HEADER_OFFSET
-        );
-        contactFormReference.current.contactFormRef.current.children[1].elements[0].focus();
-      }}
-    >
-      <p className="get-in-touch__text">Get In Touch</p>
-    </div>
-  );
-};
+class GetInTouch extends React.Component {
+  state = {
+    isRedirect: false
+  };
+
+  // if (props.contactRef === undefined) {
+  handleClick = () => {
+    if (this.props.contactRef) {
+      const contactFormReference = this.props.contactRef;
+      window.scrollTo(
+        0,
+        contactFormReference.current.contactFormRef.current.offsetTop -
+          HEADER_OFFSET
+      );
+      contactFormReference.current.contactFormRef.current.children[1].elements[0].focus();
+    } else {
+      this.setState({ isRedirect: true });
+    }
+  };
+
+  render() {
+    if (this.state.isRedirect) {
+      return <Redirect to="/contact" />;
+    } else {
+      return (
+        <div className="get-in-touch" onClick={this.handleClick}>
+          <p className="get-in-touch__text">Get In Touch</p>
+        </div>
+      );
+    }
+  }
+}
 
 export default GetInTouch;
