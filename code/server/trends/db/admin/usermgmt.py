51dcd5cdf7ef9c3b4307d797f63550234fe2c210
ClintonM0 <clinton.mo342@gmail.com>
2019-10-24 20:36:42 +1100

    Test user added in database setup

--- code/server/trends/db/admin/usermgmt.py
+++ code/server/trends/db/admin/usermgmt.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 
 def create_user(username, password, firstname, lastname, email):
-    u = User.objects.get(username, email, password)
+    u = User.objects.create_user(username, email, password)
     u.first_name = firstname
     u.last_name = lastname
     u.save()
