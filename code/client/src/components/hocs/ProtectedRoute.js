3c58d66ca4b1536662113d84cd9d25272ab48753
Cameron Williamson <cwilliamson@transformd.com>
2019-10-27 13:27:53 +1100

    add admin routes

---
+++ code/client/src/components/hocs/ProtectedRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ isAllowed, ...props }) =>
+  isAllowed ? <Route {...props} /> : <Redirect to="/login" />;
+
+export default ProtectedRoute;
