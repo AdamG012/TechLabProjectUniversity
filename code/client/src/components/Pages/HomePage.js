a8cb17ac0bc52ecf2ec2ba668c34961d78d6d4ec
Cameron Williamson <cwilliamson@transformd.com>
2019-10-27 11:55:41 +1100

    add fade on scroll

--- code/client/src/components/Pages/HomePage.js
+++ code/client/src/components/Pages/HomePage.js
@@ -3,7 +3,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
 import HeaderBanner from "../HeaderBanner";
 import ArticlesContainer from "../../containers/ArticlesContainer";
 import HeroBanner from "../HeroBanner";
@@ -14,59 +13,59 @@ import Footer from "../Footer";
 import Search from "../Search";
 import ArticleFilter from "../ArticleFilter";
 
-
 const HomePage = () => {
   const contactFormRef = React.createRef();
   return (
+    <div className="overlay" id="overlay">
       <Container fluid>
-          <Row>
-              <Col className="black-background">
-                  <HeaderBanner contactRef={contactFormRef} />
-              </Col>
-          </Row>
-          <Row>
-              <Col className="black-background">
-                  <HeroBanner />
-              </Col>
-          </Row>
-          <Row>
-              <Col>
-                  <div className="homepage__content">
-                      <div className="homepage__content-leftColumn">
-                          <div className="post-filter-and-search">
-                              <Search />
-                          </div>
-                          <ArticlesContainer />
-                      </div>
-                  </div>
-              </Col>
-              <Col>
-                  <div className="homepage__content">
-                              <SideBannerContainer />
-                  </div>
-              </Col>
-          </Row>
-          <Row>
-              <Col className="black-background">
-                      <div className="homepage__contact">
-                          <ContactForm ref={contactFormRef} />
-                      </div>
-              </Col>
-              <Col className="black-background">
-                      <div className="homepage__map">
-                          <Map />
-                      </div>
-              </Col>
-          </Row>
-          <Row>
-              <Col className="black-background">
-                <div>
-                    <Footer />
+        <Row>
+          <Col className="black-background">
+            <HeaderBanner contactRef={contactFormRef} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="black-background">
+            <HeroBanner />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="homepage__content">
+              <div className="homepage__content-leftColumn">
+                <div className="post-filter-and-search">
+                  <Search />
                 </div>
-              </Col>
-          </Row>
+                <ArticlesContainer />
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className="homepage__content">
+              <SideBannerContainer />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="black-background">
+            <div className="homepage__contact">
+              <ContactForm ref={contactFormRef} />
+            </div>
+          </Col>
+          <Col className="black-background">
+            <div className="homepage__map">
+              <Map />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="black-background">
+            <div>
+              <Footer />
+            </div>
+          </Col>
+        </Row>
       </Container>
-
+    </div>
   );
 };
 
