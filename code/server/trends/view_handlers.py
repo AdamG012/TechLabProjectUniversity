b479e45d658910460998d5125741853056e8e38f
ClintonM0 <clinton.mo342@gmail.com>
2019-11-03 15:14:49 +1100

    Tags correction

--- code/server/trends/view_handlers.py
+++ code/server/trends/view_handlers.py
@@ -164,7 +164,7 @@ def handle_article_edit(article_id, title, author, abstract, body, date, time_to
             time_to_read=ttr,
             image=image_file
     ):
-        article_admin.set_article_tags(article_id, [x.lower() for x in tags.strip()])
+        article_admin.set_article_tags(article_id, [x.lower().strip() for x in tags])
         return JsonResponse({'success': 'true'})
 
     return JsonResponse({'success': 'false'})
