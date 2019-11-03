ad477a085c18e1bce13d00ed061d2c06ffa82171
Cameron Williamson <cwilliamson@transformd.com>
2019-11-03 14:52:35 +1100

    update article view styles

--- code/client/src/components/Article.js
+++ code/client/src/components/Article.js
@@ -1,23 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Row, Col, Container } from "react-bootstrap";
 
 class Article extends React.Component {
   render() {
     const html = { __html: this.props.content };
     return (
-      <div className="article">
-        <h6>Title</h6>
-        <p>{this.props.title}</p>
-        <h6>Author</h6>
-        <p>{this.props.author}</p>
-        <h6>Time to read</h6>
-        <p>{this.props.timeToRead} min</p>
-        <h6>Content</h6>
-        <div
-          className="article__content-container"
-          dangerouslySetInnerHTML={html}
-        ></div>
-      </div>
+      <Container fluid className="article">
+        <Row className="article__row">
+          <Col className="article__col">
+            <h3 className="h3">{this.props.title}</h3>
+          </Col>
+        </Row>
+        <Row className="article__row">
+          <Col className="article__col">
+            <h6>Written by: {this.props.author}</h6>
+          </Col>
+          <Col className="article__col">
+            <h6>Time to read: {this.props.timeToRead} min</h6>
+          </Col>
+        </Row>
+        <Row className="article__row">
+          <Col className="article__col">
+            <div
+              className="article__content-container"
+              dangerouslySetInnerHTML={html}
+            ></div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
